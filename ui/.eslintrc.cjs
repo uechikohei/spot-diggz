e5c4ee24abc6d17e353baef0abbd47ee6bcc0479
321dev <52321565+uechikohei@users.noreply.github.com>
2025-12-01 15:25:28 +0900

    chore: lint/clippyエラー修正
    - React UIのESLint設定からreact-refresh推奨設定を外し、プラグイン定義を整理
    - FirestoreリポジトリのドキュメントID抽出でnext_back()を使用しClippy警告を解消

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -5,10 +5,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react-refresh/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
